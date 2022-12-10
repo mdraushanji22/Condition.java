95827576f5eccd4e863fbfaa6eb76addddac3aaf
mdraushanji22 <90023835+mdraushanji22@users.noreply.github.com>
2022-12-10 20:14:26 +0530

    Initial commit

---
+++ src/Condition.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class Condition {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        int age= sc.nextInt();
+        if (age>18){
+            System.out.println("Adult");
+
+        }
+        else{
+            System.out.println("Not Adult");
+        }
+
+    }
+}
